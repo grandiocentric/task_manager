71b3972b6c06cc959bd78f1b108a6679367b39d5
Manu Grandío <manuel.grandio@centricsoftware.com>
2024-01-24 16:57:00 +0100

    create task UI

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import get_tasks
+from .views import create_task, get_tasks
 
 urlpatterns = [
     path("", get_tasks, name="tasks"),
+    path("create/", create_task, name="create_task"),
 ]
--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -2,23 +2,28 @@ from datetime import datetime, timedelta
 
 from django.shortcuts import render
 
+tasks = [
+    dict(
+        date=datetime.now(),
+        name="Task One",
+    ),
+    dict(
+        date=datetime.now() - timedelta(days=1),
+        name="Task Two",
+    ),
+    dict(
+        date=datetime.now() - timedelta(days=3),
+        name="Task Three",
+    ),
+]
+
 
 def get_tasks(request):
-    tasks = [
-        dict(
-            date=datetime.now(),
-            name="Task One",
-            priority=1,
-        ),
-        dict(
-            date=datetime.now() - timedelta(days=1),
-            name="Task Two",
-            priority=3,
-        ),
-        dict(
-            date=datetime.now() - timedelta(days=3),
-            name="Task Three",
-            priority=2,
-        ),
-    ]
     return render(request, "tasks/list.html", {"tasks": tasks})
+
+
+def create_task(request):
+    if request.method == "GET":
+        return render(request, "tasks/create_partial.html")
+    elif request.method == "POST":
+        return render(request, "tasks/list_partial.html", {"tasks": tasks})
