70496de37ec3c4a2d110e534395534ba020d6dd1
Manu Grandío <manuel.grandio@centricsoftware.com>
2024-01-24 17:09:31 +0100

    delete task

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import create_task, get_tasks
+from .views import create_task, delete_task, get_tasks
 
 urlpatterns = [
     path("", get_tasks, name="tasks"),
     path("create/", create_task, name="create_task"),
+    path("<int:task_id>/delete/", delete_task, name="delete_task"),
 ]
--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,19 +1,23 @@
 from datetime import datetime, timedelta
 
+from django.http import HttpResponse
 from django.shortcuts import render
 
 tasks = [
     dict(
         date=datetime.now(),
         name="Task One",
+        id=1,
     ),
     dict(
         date=datetime.now() - timedelta(days=1),
         name="Task Two",
+        id=2,
     ),
     dict(
         date=datetime.now() - timedelta(days=3),
         name="Task Three",
+        id=3,
     ),
 ]
 
@@ -27,3 +31,7 @@ def create_task(request):
         return render(request, "tasks/create_partial.html")
     elif request.method == "POST":
         return render(request, "tasks/list_partial.html", {"tasks": tasks})
+
+
+def delete_task(request, task_id):
+    return HttpResponse()
