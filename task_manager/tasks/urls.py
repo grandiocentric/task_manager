faf0907139b473404d2edbfe935ce74fb277f66f
Manu Grandío <manuel.grandio@centricsoftware.com>
2024-01-24 16:14:12 +0100

    list of fake tasks

---
+++ task_manager/tasks/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from .views import get_tasks
+
+urlpatterns = [
+    path("", get_tasks, name="tasks"),
+]
--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,3 +1,24 @@
+from datetime import datetime, timedelta
+
 from django.shortcuts import render
 
-# Create your views here.
+
+def get_tasks(request):
+    tasks = [
+        dict(
+            date=datetime.now(),
+            name="Task One",
+            priority=1,
+        ),
+        dict(
+            date=datetime.now() - timedelta(days=1),
+            name="Task Two",
+            priority=3,
+        ),
+        dict(
+            date=datetime.now() - timedelta(days=3),
+            name="Task Three",
+            priority=2,
+        ),
+    ]
+    return render(request, "tasks/list.html", {"tasks": tasks})
