8952c1ab6ca37a03d667ca229c27194d65a48788
Manu Grandío <manuel.grandio@centricsoftware.com>
2024-01-25 15:32:24 +0100

    fixes

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -5,6 +5,6 @@ from .views import create_task, delete_task, get_tasks, update_task
 urlpatterns = [
     path("", get_tasks, name="tasks"),
     path("create/", create_task, name="create_task"),
-    path("<int:task_id>/delete/", delete_task, name="delete_task"),
     path("<int:task_id>/update/", update_task, name="update_task"),
+    path("<int:task_id>/delete/", delete_task, name="delete_task"),
 ]
